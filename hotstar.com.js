73e9e426f150c7470cced14f076811b37af2ac3f
Anbarasan <anbarasanmbbs@gmail.com>
2020-04-01 22:25:50 +0530

    Update changes

--- hotstar.com.js
+++ hotstar.com.js
@@ -1,23 +1,44 @@
 /* Started to set video quality HIGH when page opens */
 
 var defaultQuality = "High";
-var videoQualityMenu = document.getElementsByClassName("video-quality-menu");
+var repeatCounter = 1;
+var maxCounter = 5;
 
-if(videoQualityMenu && videoQualityMenu.length > 0)
-{
-    var videoQualityMenuDiv = videoQualityMenu[0]
-    var optionContainer = videoQualityMenuDiv.getElementsByClassName("option");
-    if(optionContainer)
+var videoQualitDefaultInterval = setInterval(function(){
+    var result = setVideoQuality(defaultQuality);
+    if(result === true || repeatCounter > maxCounter)
     {
-         for(var i=0;i<optionContainer.length;i++)
-         {
-             var option = optionContainer[i];
-             if(option.textContent == defaultQuality)
-             {
-                 option.click()
-             }
-          }
+        console.log("Finished after: " + repeatCounter);
+        clearInterval(videoQualitDefaultInterval);
     }
-}
+    else
+    {
+        console.log("Repeating quality set: " + repeatCounter)
+        repeatCounter++;
+    }
+}, 1000);
 
+
+function setVideoQuality(quality)
+{
+    var videoQualityMenu = document.getElementsByClassName("video-quality-menu");
+    if(videoQualityMenu && videoQualityMenu.length > 0)
+    {
+        var videoQualityMenuDiv = videoQualityMenu[0];
+        var optionContainer = videoQualityMenuDiv.getElementsByClassName("option");
+        if(optionContainer && optionContainer.length > 0)
+        {
+            for(var i=0;i<optionContainer.length;i++)
+            {
+                var option = optionContainer[i];
+                if(option.textContent == quality)
+                {
+                    option.click();
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
 /* End video quality update*/
