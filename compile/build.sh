95e4ec7e9c49ba9b7248820fe9ac9705723bdf21
Anbarasan <anbarasanmbbs@gmail.com>
2020-06-03 18:42:57 +0530

    option page updated

--- compile/build.sh
+++ compile/build.sh
@@ -3,7 +3,7 @@ rm -R target/
 mkdir -p target/
 cp -R ../extension/ target/
 
-sed -i '28i "applications": {"gecko": {"id": "anbarasanmbbs@gmail.com","strict_min_version": "53.0"}},' target/extension/manifest.json
+sed -i '2i "applications": {"gecko": {"id": "anbarasanmbbs@gmail.com","strict_min_version": "53.0"}},' target/extension/manifest.json
 cd ./target/extension/
 jar -cfM ../extension.zip *
 cd ..
